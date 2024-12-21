0ec25d3be414ede0895a06d9dcf4fb2425165e5e
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-21 18:18:42 +0000

    feat: add dashboard app with initial structure and configuration

--- webapp/settings.py
+++ webapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'notificaciones',
     'redes_neuronales',
     'usuarios',
+    'dashboard',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
