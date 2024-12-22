0ac5df02366f7e0ded28f30f29c69d10698c2f93
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-22 02:28:46 +0000

    feat: implement login form handling in index view with redirect on success

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from usuarios.forms import LoginForm
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'usuarios/index.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_proyectos')
+    else:
+        form = LoginForm()
+    return render(request, 'usuarios/index.html', {'form': form})
