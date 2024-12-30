8ea670ddb26b8d86ea4ca042284cf0768eaa2b29
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-30 03:29:44 +0000

    🔧 configure database settings to use environment variables for improved security

--- webapp/settings.py
+++ webapp/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,11 +93,11 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbtest_webapp_pm', # Nombre de la base de datos
-        'USER': 'development',
-        'PASSWORD': '123456',
-        'HOST': 'db-webapp-PM',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
