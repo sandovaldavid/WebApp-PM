3970035872fd84437c5d0188482566b2d13183c8
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-30 21:23:22 +0000

    ✨ update URL routing; set dashboard as the index view for notifications

--- notificaciones/urls.py
+++ notificaciones/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = 'notificaciones'
 urlpatterns = [
-  path('', views.index, name='index'),
   path('crear-notificacion/', views.crear_notificacion, name='crear_notificacion'),
   path('crear-alerta/', views.crear_alerta, name='crear_alerta'),
-  path('dashboard/', views.dashboard, name='dashboard'),
+  path('', views.dashboard, name='index'),
 ]
