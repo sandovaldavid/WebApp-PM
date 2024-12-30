eac234176281ab410a7ebb6b07df36fc3c928052
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-30 21:52:13 +0000

    ✨ update URL routing; add detail and mark notification paths for improved functionality

--- notificaciones/urls.py
+++ notificaciones/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'notificaciones'
+app_name = "notificaciones"
 urlpatterns = [
-  path('crear-notificacion/', views.crear_notificacion, name='crear_notificacion'),
-  path('crear-alerta/', views.crear_alerta, name='crear_alerta'),
-  path('', views.dashboard, name='index'),
+    path("crear-notificacion/", views.crear_notificacion, name="crear_notificacion"),
+    path("crear-alerta/", views.crear_alerta, name="crear_alerta"),
+    path("", views.dashboard, name="index"),
+    path("notificacion/<int:id>/", views.detalle_notificacion, name="detalle_notificacion"),
+    path('alerta/<int:id>/', views.detalle_alerta, name='detalle_alerta'),
+    path('notificacion/<int:id>/marcar/', views.marcar_notificacion, name='marcar_notificacion'),
 ]
