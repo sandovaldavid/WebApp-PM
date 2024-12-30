9624df39ffb5fdfef11a1ccb84da9c444d75041e
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-30 21:09:50 +0000

    ✨ add URL routes for creating notifications, alerts, and dashboard view

--- notificaciones/urls.py
+++ notificaciones/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'notificaciones'
 urlpatterns = [
   path('', views.index, name='index'),
+  path('crear-notificacion/', views.crear_notificacion, name='crear_notificacion'),
+  path('crear-alerta/', views.crear_alerta, name='crear_alerta'),
+  path('dashboard/', views.dashboard, name='dashboard'),
 ]
