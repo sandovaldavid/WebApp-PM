b9daa14317815989931d228e4977b20eb927d34a
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-30 04:51:18 +0000

    🗑️ remove unused models and related code from various apps to streamline the project

--- gestion_equipos/models.py
+++ gestion_equipos/models.py
@@ -1,29 +1,2 @@
 from django.db import models
 
-class Equipo(models.Model):
-    idEquipo = models.AutoField(primary_key=True)
-    nombreEquipo = models.CharField(max_length=255)
-    descripcion = models.TextField(blank=True, null=True)
-    fechaCreacion = models.DateTimeField(auto_now_add=True)
-    fechaModificacion = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.nombreEquipo
-
-    class Meta:
-        db_table = 'equipo'
-        verbose_name = 'Equipo'
-        verbose_name_plural = 'Equipos'
-
-class Miembro(models.Model):
-    idMiembro = models.AutoField(primary_key=True)
-    recurso = models.ForeignKey('gestion_recursos.Recurso', on_delete=models.CASCADE, related_name="miembros")
-    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name="miembros")
-
-    def __str__(self):
-        return f"{self.recurso.nombreRecurso} - {self.equipo.nombreEquipo}"
-
-    class Meta:
-        db_table = 'miembro'
-        verbose_name = 'Miembro'
-        verbose_name_plural = 'Miembros'
--- redes_neuronales/models.py
+++ redes_neuronales/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 # from dashboard.models import Proyecto  # Esto causa la importación circular
 
-class ModeloRedNeuronal(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True, null=True)
-    proyecto = models.ForeignKey('dashboard.Proyecto', on_delete=models.CASCADE, related_name="modelos_redes")
-    fecha_entrenamiento = models.DateField()
-    accuracy = models.FloatField()
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        db_table = 'modelo_red_neuronal'
-        ordering = ['id']
-        verbose_name = 'Modelo de Red Neuronal'
-        verbose_name_plural = 'Modelos de Redes Neuronales'
