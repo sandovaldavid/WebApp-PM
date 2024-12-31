c8081f30bf6f49d774c505dacfc417324c0b8b55
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-31 01:16:34 +0000

    ✨ add detalle-tarea route to gestion_tareas URLs; enhance task management functionality

--- gestion_tareas/urls.py
+++ gestion_tareas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'gestion_tareas'
+app_name = "gestion_tareas"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('tareas_programadas/', views.tareas_programadas, name='tareas_programadas'),
+    path("", views.index, name="index"),
+    path("tareas_programadas/", views.tareas_programadas, name="tareas_programadas"),
+    path("detalle-tarea/<int:id>/", views.detalle_tarea, name="detalle_tarea"),
 ]
