1afd271e4a40080b115824f0b6dcb494f1aef265
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-31 02:42:07 +0000

    ✨ add user registration and login routes; enhance user management functionality

--- gestion_usuarios/urls.py
+++ gestion_usuarios/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'gestionUsuarios'
 urlpatterns = [
-    path('', views.lista_usuarios, name='lista_usuarios'),
-    path('crear/', views.crear_usuario, name='crear_usuario'),
+    path("", views.lista_usuarios, name="lista_usuarios"),
+    path("crear/", views.crear_usuario, name="crear_usuario"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
 ]
