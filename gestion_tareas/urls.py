0dfdd1d3d391c0cb5d91704e8fb7ae876c8ca8aa
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-31 10:45:28 +0000

    ✨ add route for editing tasks

--- gestion_tareas/urls.py
+++ gestion_tareas/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("crear-tarea/", views.crear_tarea, name="crear_tarea"),
     path("tareas_programadas/", views.tareas_programadas, name="tareas_programadas"),
     path("detalle-tarea/<int:id>/", views.detalle_tarea, name="detalle_tarea"),
+    path("editar-tarea/<int:id>/", views.editar_tarea, name="editar_tarea"),
 ]
