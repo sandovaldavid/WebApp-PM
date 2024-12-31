d7105d2b0c24a3b73e5983d3eebdc3abc57611a7
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-31 09:11:32 +0000

    ✨ add URL route for task creation functionality

--- gestion_tareas/urls.py
+++ gestion_tareas/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "gestion_tareas"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("crear-tarea/", views.crear_tarea, name="crear_tarea"),
     path("tareas_programadas/", views.tareas_programadas, name="tareas_programadas"),
     path("detalle-tarea/<int:id>/", views.detalle_tarea, name="detalle_tarea"),
 ]
