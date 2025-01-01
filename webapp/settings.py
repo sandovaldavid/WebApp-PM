e1f8d14905104c34418a4b7bfb1de9ca1b26ac5b
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-01 14:31:19 -0500

    🔧 set LOGIN_URL to "usuarios:login" in settings.py for improved authentication routing

--- webapp/settings.py
+++ webapp/settings.py
@@ -150,3 +150,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = "usuarios:login"
