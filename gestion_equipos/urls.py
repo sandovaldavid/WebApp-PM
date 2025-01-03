af1fe86dc99db2cb57aff53f2ce9454687e393ca
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-03 02:33:17 -0500

    ✨  add URL patterns for managing teams, including member management, creation, editing, deletion, and detail views

--- gestion_equipos/urls.py
+++ gestion_equipos/urls.py
@@ -4,4 +4,21 @@ from . import views
 app_name = "gestion_equipos"
 urlpatterns = [
     path("", views.index, name="index"),
+    path(
+        "<int:equipo_id>/miembros/", views.gestionar_miembros, name="gestionar_miembros"
+    ),
+    path(
+        "equipo/<int:equipo_id>/miembros/", views.lista_miembros, name="lista_miembros"
+    ),
+    path("crear/equipo/", views.crear_equipo, name="crear_equipo"),
+    path("editar/<int:equipo_id>/equipo/", views.editar_equipo, name="editar_equipo"),
+    path(
+        "eliminar/<int:equipo_id>/equipo/",
+        views.eliminar_equipo,
+        name="eliminar_equipo",
+    ),
+    path("lista-equipo/", views.lista_equipos, name="lista_equipos"),
+    path(
+        "detalle-equipo/<int:equipo_id>/", views.detalle_equipo, name="detalle_equipo"
+    ),
 ]
