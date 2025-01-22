10a5360af699aaab5a070e25e6540707f565e78f
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-21 23:10:03 -0500

    ✨ Mejorar la función de generación de usuarios: agregar hash de contraseña, fecha de creación aleatoria y confirmar usuario.

--- script/random_user.py
+++ script/random_user.py
@@ -1,6 +1,8 @@
 from faker import Faker
 import uuid
 from dashboard.models import Usuario
+from django.contrib.auth.hashers import make_password
+from django.utils import timezone
 
 def generar_usuarios(num_usuarios, rol_user):
     fake = Faker()
@@ -13,17 +15,26 @@ def generar_usuarios(num_usuarios, rol_user):
             username = fake.user_name()
             email = fake.email()
             contrasena = fake.password(length=12)
+            hashed_password = make_password(contrasena)
             rol = rol_user
             token = str(uuid.uuid4())
+                        # Generar fecha aleatoria entre hace 1 año y hoy
+            fechacreacion = fake.date_time_between(
+                start_date='-1y',
+                end_date='now',
+                tzinfo=timezone.get_current_timezone()
+            )
 
             # Crear el usuario
             usuario = Usuario.objects.create(
                 nombreusuario=nombreusuario,
                 username=username,
                 email=email,
-                contrasena=contrasena,
+                contrasena=hashed_password,
                 rol=rol,
-                token=token
+                token=token,
+                fechacreacion=fechacreacion,
+                confirmado = True
             )
             usuarios_creados.append(usuario)
             print(f"Usuario creado: {username}")
