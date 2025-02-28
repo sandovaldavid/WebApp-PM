a67ba7a76dc4e2769a1551b8407cc2e414eeab1a
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-27 23:01:25 -0500

    🔥 remove Prettier formatting step from format script

--- script/format.sh
+++ script/format.sh
@@ -3,7 +3,4 @@
 echo "Formateando archivos Python con Black..."
 black .
 
-echo "Formateando archivos HTML, JS y CSS con Prettier..."
-npx prettier --write "**/*.{js,html,css}"
-
 echo "¡Formateo completado!"
