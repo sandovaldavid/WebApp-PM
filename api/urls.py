ce5de3eed7c81ab8df917ec1ac61a35bf87a67ab
sandovaldavid <sandovaldavid2201@gmail.com>
2025-03-23 01:14:58 -0500

    ✨ Add Requerimiento route to API for enhanced requirement management

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import usuario_views, proyecto_views, tarea_views, equipo_views
+from api.views import usuario_views, proyecto_views, tarea_views, equipo_views, requerimiento_views
 from api.views.auth_views import LoginView
 from rest_framework.authtoken import views as token_views
 
@@ -10,6 +10,7 @@ router.register(r"usuarios", usuario_views.UsuarioViewSet)
 router.register(r"proyectos", proyecto_views.ProyectoViewSet)
 router.register(r"tareas", tarea_views.TareaViewSet)
 router.register(r"equipos", equipo_views.EquipoViewSet)
+router.register(r"requerimientos", requerimiento_views.RequerimientoViewSet)
 
 # The API URLs are determined automatically by the router
 urlpatterns = [
