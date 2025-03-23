c40e15c8726c007778043099ea41792e518021fc
sandovaldavid <sandovaldavid2201@gmail.com>
2025-03-22 22:13:03 -0500

    ✨ Add Tarea and Equipo routes to API for enhanced task and team management

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import usuario_views, proyecto_views
+from api.views import usuario_views, proyecto_views, tarea_views, equipo_views
 from api.views.auth_views import LoginView
 from rest_framework.authtoken import views as token_views
 
 # Create a router and register our viewsets with it
 router = DefaultRouter()
-router.register(r'usuarios', usuario_views.UsuarioViewSet)
-router.register(r'proyectos', proyecto_views.ProyectoViewSet)
+router.register(r"usuarios", usuario_views.UsuarioViewSet)
+router.register(r"proyectos", proyecto_views.ProyectoViewSet)
+router.register(r"tareas", tarea_views.TareaViewSet)
+router.register(r"equipos", equipo_views.EquipoViewSet)
 
 # The API URLs are determined automatically by the router
 urlpatterns = [
-    path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls')),
-    path('login/', LoginView.as_view(), name='api-login'),
-    path('token-auth/', token_views.obtain_auth_token, name='api-token-auth'),
+    path("", include(router.urls)),
+    path("auth/", include("rest_framework.urls")),
+    path("login/", LoginView.as_view(), name="api-login"),
+    path("token-auth/", token_views.obtain_auth_token, name="api-token-auth"),
 ]
